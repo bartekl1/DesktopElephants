615fbca96474c4943597f5ed147a4ad15a72b094
slonindyjski <barteklorencslon@gmail.com>
2023-06-02 19:09:35 +0200

    v 1.1

--- elephant.py
+++ elephant.py
@@ -5,6 +5,7 @@ import random
 import sys
 import os
 
+
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
     try:
@@ -14,6 +15,13 @@ def resource_path(relative_path):
         base_path = os.path.abspath(".")
 
     return os.path.join(base_path, relative_path)
+
+
+def change_window_position():
+    x, y = random.randint(0, m.width - 300), random.randint(0, m.height - 200)
+    window.geometry(f'+{x}+{y}')
+    window.after(2000, change_window_position)
+
 
 m = screeninfo.get_monitors()[0]
 
@@ -30,6 +38,6 @@ img_tk = ImageTk.PhotoImage(img)
 label = tk.Label(window, image=img_tk, bg='white')
 label.pack()
 
-window.geometry(f'+{random.randint(0, m.width - 300)}+{random.randint(0, m.height - 200)}')
+change_window_position()
 
 window.mainloop()
